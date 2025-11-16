72b164d55f57c2226fa9b3c0132f9bd373441903
earezki <arezki.elmehdi@gmail.com>
2025-11-16 22:24:41 +0100

    add subscriptions

--- server/api.py
+++ server/api.py
@@ -38,7 +38,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["GET", "OPTIONS"],
+    allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["*"],
 )
 
